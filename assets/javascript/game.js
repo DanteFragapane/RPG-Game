45ef550500c341992a0bfe87a3ff59e214b25fbe
Dante Fragapane <dante.fragapane@gmail.com>
2019-05-28 13:18:31 -0400

    Moved repeated code to functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,6 +23,32 @@ $("document").ready(() => {
         };
     }
 
+
+    const createEnemies = function (characterList, active) {
+        for (let i = 0; i < characterList.length; i++) {
+            if (characterList[i].name != active.name) {
+                let character = characterList[i];
+                div = $("<div class='character'>") // Create the div.character
+                div.attr("number", i);
+                div.append($("<img>").attr('src', `assets/images/${character.name}.jpg`)) // Create ``img`` as child of ``div``
+                div.append($("<text>", {
+                    text: character.fullname
+                })); // Create ``text`` as child of ``div``
+                $("#enemies").append(div); // Actually add the ``div`` element as a child of ``#characters``
+            }
+        };
+    }
+
+    const createDiv = function(character, i) {
+        div = $("<div class='character'>") // Create the div.character
+        div.attr("number", i);
+        div.append($("<img>").attr('src', `assets/images/${character.name}.jpg`)) // Create ``img`` as child of ``div``
+        div.append($("<text>", {
+            text: character.fullname
+        })); // Create ``text`` as child of ``div``
+        return div;
+    }
+
     // Creating the characters
     const obi = new Character("obi", "Obi Wan Kanobi", 100, 6, 4);
     const luke = new Character("luke", "Luke Skywalker", 150, 8, 4);
@@ -50,33 +76,27 @@ $("document").ready(() => {
         //     $("#characters").append(div); // Actually add the ``div`` element as a child of ``#characters``
         // };
     }
+
     for (let i = 0; i < characters.length; i++) {
         let character = characters[i];
-        div = $("<div class='character'>") // Create the div.character
-        div.attr("number", i);
-        div.append($("<img>").attr('src', `assets/images/${character.name}.jpg`)) // Create ``img`` as child of ``div``
-        div.append($("<text>", {
-            text: character.fullname
-        })); // Create ``text`` as child of ``div``
+        let div = createDiv(character, i);
         $("#characters").append(div); // Actually add the ``div`` element as a child of ``#characters``
     };
 
 
 
 
-
-
-
-
+    // Clicking a character in the character section sets it as active
     $(".character").click(function () {
-        console.log(event);
         activeCharacter = characters[$(this).attr("number")];
-        console.log(activeCharacter);
-
-
+        // Hiding all but the active character
+        $(".character").each(function () { // jQuery's forEach
+            if (characters[$(this).attr("number")] != activeCharacter) { // If the name of the character is NOT the active character's name
+                $(this).css("display", "none");
+            }
+        });
+        createEnemies(characters, activeCharacter)
     })
-
-
 
 
     reset();
