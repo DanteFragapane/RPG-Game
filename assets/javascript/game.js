876eb992db2f58cf69126b6eeb41e8977cec1cb1
Dante Fragapane <dante.fragapane@gmail.com>
2019-05-27 21:04:37 -0400

    More work, including:    adding new attribute 'name' to the constructor    dynamically adding images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,44 @@
 $("document").ready(() => {
 
-    function Player(health, attackPower, counterPower) {
+
+    // Character object constructor
+    function Character(name, health, attackPower, counterPower) {
+        this.name = name;
         this.health = health;
         this.attackPower = attackPower;
+        this.increment = attackPower;
         this.counterPower = counterPower;
 
-        this.attack = function(enemy) {
+        this.attack = function (enemy) {
             this.health -= enemy.attacked();
-        }
+            this.attackPower += this.increment;
+        };
 
-        this.attacked = function(damage) {
+        this.attacked = function (damage) {
             this.health -= damage;
             return this.counterPower;
-        }
+        };
     }
 
-    function Enemy(health, counterPower) {
-        this.health = health;
-        this.counterPower = counterPower;
-    }
+    // Creating the characters
+    const obi = new Character("obi", 100, 6, 4);
+    const luke = new Character("luke", 150, 8, 4);
+    const sidious = new Character("sidious", 100, 25);
+    const maul = new Character("maul", 125, 10);
 
+    characters = [obi, luke, sidious, maul];
 
-    const obi = new Player(100, 6, 4);
-    const luke = new Player(150, 8, 4);
-    const sidious = new Enemy(100, 25);
-    const maul = new Enemy(125, 10);
-
-    console.log(obi.health);
+    // Quick tests for checking attack
+    console.log(`${obi.health} ${obi.attackPower}`);
     obi.attack(luke);
-    console.log(obi.health);
+    console.log(`${obi.health} ${obi.attackPower}`)
 
-    characters = [];
+
+    characters.forEach(character => {
+        div = $("<div class='character'>").append($("<img>").attr('src', `assets/images/${character.name}.jpg`)) // Create the div, append a <img> with attributes of 'src' being the headshot
+        $("#characters").append(div);
+    });
+
 
 
 })
